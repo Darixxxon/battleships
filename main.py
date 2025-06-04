2bb6f27fa81da84a4bc950b70aec504b9a9a8d92
MaksPek03 <maks.pek90@gmail.com>
2025-06-04 18:11:15 +0200

    changes

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from board import *
 import client
 import server
+import pygame
 
 if __name__ == "__main__":
-   running = True
-   board = Board()
+    running = True
+    board = Board()
 
-   (type, host, port) = board.start_menu()
-   if type == "Server":
-      server.main_server(board, host, port)
-   elif type == "Client":
-      client.main_client(board, host, port)
-       
-   board.determine_enemy_square_color(0,0)
-   pygame.quit()
-   quit()
+    pygame.init()
+    clock = pygame.time.Clock() 
+
+    (type, host, port) = board.start_menu()
+    
+    if type == "Server":
+        server.main_server(board, host, port)
+    elif type == "Client":
+        client.main_client(board, host, port)
+    board.determine_enemy_square_color(0, 0)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                quit()
+        clock.tick(20)
+
+    pygame.quit()
+    quit()
